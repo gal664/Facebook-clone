e236c1546b7c471c0472f1fe0ac2a2be02fe636d
Gal Yaniv <gal.yaniv@timetoknow.co.il>
2018-06-16 19:33:37 +0300

    added homework's bonus features: like button, edit button and posts oredering in feed (newest is first)

--- js/main.js
+++ js/main.js
@@ -27,18 +27,60 @@ function createElement(t, c, f) {
 }
 //try and turn this into a class
 function createNewPost(postText) {
-      let newPost = createElement("div", "feed-item", elements.feed);
+      let newPost = document.createElement("div");
+      newPost.className = "feed-item";
+      elements.feed.insertBefore(newPost, elements.feed.firstChild);
+
       let postHeader = createElement("div", "feed-item__header", newPost);
       let headerLeft = createElement("div", "header_left", postHeader)
-      let headerRight = createElement("div", "header_right", postHeader)
       let user_name = createElement("span", "user_name", headerLeft);
       let postTime = createElement("span", "timestamp", headerLeft);
+      let headerRight = createElement("div", "header_right", postHeader)
       let removePostBtn = createElement("button", "remove_post_btn", headerRight);
       let post = createElement("div", "post_text", newPost);
+      let postActions = createElement("div", "post_actions", newPost);
+      let likesContainer = createElement("div", "likes_container", postActions);
+      let likeBtn = createElement("button", "like_btn", likesContainer);
+      let likesCounter = createElement("span", "likes_counter", likesContainer);
+      let editPostBtn = createElement("button", "edit_post_button", postActions);
+      likeBtn.innerText = "Like";
+      likesCounter.innerText = 0;
+      editPostBtn.innerText = "Edit";
       user_name.innerText = "Gal Yaniv";
       removePostBtn.innerText = "Remove Post";
       postTime.innerText = new Date().toLocaleTimeString();
       post.innerText = postText;
       removePostBtn.addEventListener('click', (event) => { elements.feed.removeChild(newPost) })
-            return newPost;
+      editPostBtn.addEventListener('click', (event) => {
+            editPostBtn.disabled = true;
+            currentPostText = post.innerText;
+            let editPost = createElement("input", "edit_post", newPost);
+            newPost.replaceChild(editPost, post);
+            editPost.value = currentPostText;
+            editPost.focus();
+            editPost.addEventListener('keydown', (event) => {
+                  newPostText = editPost.value;
+                  if (event.key === "Enter") {
+                        editPostBtn.disabled = false;
+                        newPost.replaceChild(post, editPost);
+                        post.innerText = newPostText;
+                  }
+            })
+      })
+      // bug found: if there are more than 1 posts, the likes tuen negative on every other post.
+      // probably the userLikedPost is global for all posts, need to make it only for the specific post.
+      userLikedPost = false;
+      likeBtn.addEventListener('click', (event) => {
+            switch (userLikedPost) {
+                  case false:
+                        likesCounter.innerText++;
+                        userLikedPost = true;
+                        break;
+                  case true:
+                        likesCounter.innerText--;
+                        userLikedPost = false;
+                        break;
+            }
+      })
+      return newPost;
 }
