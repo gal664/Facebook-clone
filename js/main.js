b5ab2443118f219cf7f606c85df951cf6a2bdf3a
Gal Yaniv <gal.yaniv@timetoknow.co.il>
2018-06-17 16:15:04 +0300

    fixed bug #1, added functions to status update,decreased code repeatings.

--- js/main.js
+++ js/main.js
@@ -12,44 +12,39 @@ function init() {
             feed: document.querySelector(".main_feed__feed"),
       }
       elements.statusUpdate.addEventListener('keydown', (event) => {
-            if (event.key === "Enter") {
+            if (event.key === "Enter" && elements.statusUpdate.value !== "") {
                   postText = elements.statusUpdate.value;
                   createNewPost(postText);
                   elements.statusUpdate.value = "";
             }
       });
 }
-function createElement(t, c, f) {
+function createElement(t, c, f, txt) {
       let el = document.createElement(t);
       el.className = c;
       f.appendChild(el);
+      el.innerText = txt;
       return el;
 }
 //try and turn this into a class
 function createNewPost(postText) {
+      let userLikedPost = false;
       let newPost = document.createElement("div");
       newPost.className = "feed-item";
       elements.feed.insertBefore(newPost, elements.feed.firstChild);
 
-      let postHeader = createElement("div", "feed-item__header", newPost);
-      let headerLeft = createElement("div", "header_left", postHeader)
-      let user_name = createElement("span", "user_name", headerLeft);
-      let postTime = createElement("span", "timestamp", headerLeft);
-      let headerRight = createElement("div", "header_right", postHeader)
-      let removePostBtn = createElement("button", "remove_post_btn", headerRight);
-      let post = createElement("div", "post_text", newPost);
-      let postActions = createElement("div", "post_actions", newPost);
-      let likesContainer = createElement("div", "likes_container", postActions);
-      let likeBtn = createElement("button", "like_btn", likesContainer);
-      let likesCounter = createElement("span", "likes_counter", likesContainer);
-      let editPostBtn = createElement("button", "edit_post_button", postActions);
-      likeBtn.innerText = "Like";
-      likesCounter.innerText = 0;
-      editPostBtn.innerText = "Edit";
-      user_name.innerText = "Gal Yaniv";
-      removePostBtn.innerText = "Remove Post";
-      postTime.innerText = new Date().toLocaleTimeString();
-      post.innerText = postText;
+      let postHeader = createElement("div", "feed-item__header", newPost, "");
+      let headerLeft = createElement("div", "header_left", postHeader, "")
+      let user_name = createElement("span", "user_name", headerLeft, "Gal Yaniv");
+      let postTime = createElement("span", "timestamp", headerLeft, new Date().toLocaleTimeString());
+      let headerRight = createElement("div", "header_right", postHeader, "")
+      let removePostBtn = createElement("button", "remove_post_btn", headerRight, "Remove Post");
+      let post = createElement("div", "post_text", newPost, postText, "");
+      let postActions = createElement("div", "post_actions", newPost, "");
+      let likesContainer = createElement("div", "likes_container", postActions, "");
+      let likeBtn = createElement("button", "like_btn", likesContainer, "Like");
+      let likesCounter = createElement("span", "likes_counter", likesContainer, 0);
+      let editPostBtn = createElement("button", "edit_post_button", postActions, "Edit");
       removePostBtn.addEventListener('click', (event) => { elements.feed.removeChild(newPost) })
       editPostBtn.addEventListener('click', (event) => {
             editPostBtn.disabled = true;
@@ -67,20 +62,23 @@ function createNewPost(postText) {
                   }
             })
       })
-      // bug found: if there are more than 1 posts, the likes tuen negative on every other post.
-      // probably the userLikedPost is global for all posts, need to make it only for the specific post.
-      userLikedPost = false;
+      let likesIndex = likesCounter.innerText;
       likeBtn.addEventListener('click', (event) => {
+            console.log(userLikedPost);
             switch (userLikedPost) {
                   case false:
-                        likesCounter.innerText++;
-                        userLikedPost = true;
-                        break;
+                  console.log("false");
+                  likesIndex++;
+                  userLikedPost = true;
+                  break;
                   case true:
-                        likesCounter.innerText--;
+                  console.log("true");
+                        likesIndex--;
                         userLikedPost = false;
                         break;
             }
+            console.log(likesIndex);
+            likesCounter.innerText = likesIndex;
       })
       return newPost;
 }
